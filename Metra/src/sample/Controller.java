90d61b71fb84bbce654dbcc16303ab17dd8430a9
Dmitry-coder <57069404+Dmitry-coder@users.noreply.github.com>
2020-10-01 19:22:28 +0300

    библиотека

--- Metra/src/sample/Controller.java
+++ Metra/src/sample/Controller.java
@@ -12,15 +12,36 @@ public class Controller {
     @FXML
     private Button btnScan;
 
-    @FXML
+   @FXML
     void initialize() {
+
+        String operators[] = {"=", "+", "==", "-", "*", "\\\\", "%", "**", "!=", "<>", "!==", "&", "^", "<<",
+                ">>", "<", ">", "<=", ">=", "<=>", "''", "@", "++", "--", "&&", "||", "!", "and",
+                "xor", "or", ".", "instanceof", "?:", "?", "??", "clone new", "yield from", "yield", "print", "abs",
+                "round", "ceil", "floor", "srand", "getrandmax", "rand", "base_convert", "bindec", "decbin", "dechex",
+                "min", "max", "sqrt", "log", "pow", "sin", "cos", "acos", "if", "else", "elseif", "switch", "for", "while",
+                "do", "foreach", "checkdate", "date", "getdate", "is_null", "is_integer", "is_string",
+                "gettype", "empty", "isset", };
+        //берем строку, сплитуем по пробелу, затем
+
+
 
         btnScan.setOnAction(event -> {
             String newString;
 
             newString = scanCode.getText();
 
+            newString.split(" ");
+
+            int operat;
+            int operand;
+            for (int i = 0; i < operators.length; i++){
+                
+            }
+
             System.out.println(newString);
         });
+
+
     }
 }
